b0e6e26a691d89fd4228f32c4e0da27f8cf7d8e9
Tomasz Kalata <klingaroo@gmail.com>
2019-05-02 15:39:53 -0400

    context init

---
+++ tests/context.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+p = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, p + '\\' + p.split('\\')[-1])
+sys.path.insert(0, p)
+
+
+from babel.data_sources import xlsx
