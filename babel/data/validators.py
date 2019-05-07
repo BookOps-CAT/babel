2dda712547bf39a371bd8d32e6099f619837ccb2
Tomasz Kalata <klingaroo@gmail.com>
2019-05-07 09:41:43 -0400

    normalize_isbn added

--- babel/data/validators.py
+++ babel/data/validators.py
@@ -1,13 +1,30 @@
 # validates data passed from spreadsheets
 import re
 
+p1 = re.compile(r'[12]\d{3}')
+p2 = re.compile(r'97\d{10}[xX]|97\d{11}|^\d{10}(?!\d)|^\d{9}[xX](?!\d)')
+
 
 def value2string(value):
-    return str(value)
+    if type(value) is int or type(value) is float:
+        return str(value)
+    elif type(value) is str:
+        return value
 
 
 def normalize_date(value):
-    m = re.search(
-        re.compile('\\d{4}', value))
-    if m:
-        return m.group(0)
+    value = value2string(value)
+    if value:
+        m = re.search(p1, value)
+        if m:
+            return m.group(0)
+
+
+def normalize_isbn(value):
+    value = value2string(value)
+    if value:
+        value = value.replace('-', '').replace(' ', '').replace(
+            '\n', ' ').lower().strip()
+        m = re.search(p2, value)
+        if m:
+            return m.group(0)
--- tests/test_data_validators.py
+++ tests/test_data_validators.py
@@ -4,20 +4,88 @@ import unittest
 
 from context import validators
 
+
 class TestValue2String(unittest.TestCase):
 
     def test_int_to_unicode(self):
         value = 2016
         value_as_uni = validators.value2string(value)
-        print(type(value_as_uni))
+        self.assertIsInstance(value_as_uni, str)
+
+    def test_float_to_unicode(self):
+        value = 12.99
+        value_as_uni = validators.value2string(value)
+        self.assertIsInstance(value_as_uni, str)
+
+    def test_str_to_unicode(self):
+        value = 'Маэстра'
+        value_as_uni = validators.value2string(value)
+        self.assertIsInstance(value_as_uni, str)
+
+    def test_none_returns_none(self):
+        value = None
+        value_as_uni = validators.value2string(value)
+        self.assertIsNone(value_as_uni)
 
 
 class TestNormalizeDate(unittest.TestCase):
     """Test date parsing"""
 
-    def test_int2string(self):
-        # date = validators.normalize_date(2016)
-        pass
+    def test_int_to_string(self):
+        date = validators.normalize_date(2016)
+        self.assertEqual(date, '2016')
+
+    def test_buried_date(self):
+        date = validators.normalize_date('07/12/2016')
+        self.assertEqual(date, '2016')
+
+    def test_buried_date_with_diacritics(self):
+        date = validators.normalize_date('2016年1月')
+        self.assertEqual(date, '2016')
+
+    def test_incorect_returns_none(self):
+        date = validators.normalize_date('Jul=16')
+        self.assertIsNone(date)
+
+
+class TestNormalizeISBN(unittest.TestCase):
+    """Test parsing and normalizing ISBNs"""
+
+    def test_None_returns_none(self):
+        isbn = validators.normalize_isbn(None)
+        self.assertIsNone(isbn)
+
+    def test_white_space_returns_none(self):
+        isbn = validators.normalize_isbn('')
+        self.assertIsNone(isbn)
+
+    def test_incorrect_isbn_returns_none(self):
+        isbn = validators.normalize_isbn('23980433')
+        self.assertIsNone(isbn)
+
+    def test_13_digits_upc_returns_none(self):
+        isbn = validators.normalize_isbn('5060099503825')
+        self.assertIsNone(isbn)
+
+    def test_10_digits_valid_isbn(self):
+        isbn = validators.normalize_isbn('8376723979')
+        self.assertEqual(isbn, '8376723979')
+
+    def test_13_digits_valid_isbn(self):
+        isbn = validators.normalize_isbn('9788376723976')
+        self.assertEqual(isbn, '9788376723976')
+
+    def test_stripping_dashes(self):
+        isbn = validators.normalize_isbn('978-8-37-672397-6')
+        self.assertEqual(isbn, '9788376723976')
+
+    def test_stripping_white_space(self):
+        isbn = validators.normalize_isbn(' 9788 37672397 6 ')
+        self.assertEqual(isbn, '9788376723976')
+
+    def test_value_with_line_breaks(self):
+        isbn = validators.normalize_isbn('978-8-37-672397-6  \n 8376723979')
+        self.assertEqual(isbn, '9788376723976')
 
 
 if __name__ == '__main__':
