52e5291509de88a070ddbdff30c49e5ac4dd24ae
Tomasz Kalata <klingaroo@gmail.com>
2019-05-23 16:32:33 -0400

    update & delete added

--- babel/data/datastore_worker.py
+++ babel/data/datastore_worker.py
@@ -13,12 +13,23 @@ def insert_or_ignore(session, model, **kwargs):
 
 def get_column_values(session, model, column, **kwargs):
     instances = session.query(model).filter_by(**kwargs).options(
-        load_only(column)).order_by(column).all()
+        load_only(column)).order_by(column)
     return instances
 
 
 def retrieve_record(session, model, **kwargs):
-    instance = session.query(model).filter_by(**kwargs).one()
+    instance = session.query(model).filter_by(**kwargs).first()
     return instance
+
+
+def update_record(session, model, did, **kwargs):
+    instance = session.query(model).filter_by(id=id).one()
+    for key, value in kwargs.iteritems():
+        setattr(instance, key, value)
+
+
+def delete_record(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).one()
+    session.delete(instance)
 
 
