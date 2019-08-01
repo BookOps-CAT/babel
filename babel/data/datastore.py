ef359acce70708cb63b14bc5f38f1da288b44376
Tomasz Kalata <klingaroo@gmail.com>
2019-08-01 09:16:22 -0400

    created date in carts (issue #55)

--- babel/data/datastore.py
+++ babel/data/datastore.py
@@ -614,7 +614,7 @@ def create_datastore(
     stmn = """
     CREATE VIEW carts_meta AS
         SELECT cart.did AS cart_id, cart.name AS cart_name,
-               cart.updated AS cart_date, cart.system_id AS system_id,
+               cart.created AS cart_date, cart.system_id AS system_id,
                status.name AS cart_status, user.name AS cart_owner
         FROM cart
             JOIN status ON status.did = cart.status_id
