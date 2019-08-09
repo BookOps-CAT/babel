cce2ad5be515d69aaeaad21e124b8104fb2319f9
klinga <klingaroo@gmail.com>
2019-08-09 00:20:21 -0400

    updater & new icon init

--- babel/babel.py
+++ babel/babel.py
@@ -2,22 +2,37 @@
 
 import logging
 import logging.config
-import logging.handlers
-import keyring
-from keyring.backends.Windows import WinVaultKeyring
+import os
 from tkinter.ttk import Style
 import shelve
 
+import logging.handlers
+import keyring
+from keyring.backends.Windows import WinVaultKeyring
+
+
 from gui.main import Base
+from gui.updater import Updater
 from logging_settings import DEV_LOGGING, LogglyAdapter, format_traceback
 from paths import USER_DATA
 
 
-def determine_version():
+def determine_version(directory):
+    version_fh = os.path.join(directory, 'version.txt')
     about = {}
-    with open('version.txt') as f:
+    with open(version_fh, 'r') as f:
         exec(f.read(), about)
     return about['__version__']
+
+
+def launch_updater():
+    app = Updater()
+    s = Style()
+    s.theme_use('xpnative')
+    s.configure('.', font=('device', 12))
+    # app.iconbitmap('./icons/babel.ico')
+    app.title('Babel Updater')
+    app.mainloop()
 
 
 if __name__ == '__main__':
@@ -35,11 +50,21 @@ if __name__ == '__main__':
 
     # determine version, wonder if this will work after packaging
     # into frozen binaries?
-    version = determine_version()
+    local_version = determine_version(os.getcwd())
+
+    # check if newer available
+    user_data = shelve.open(USER_DATA)
+    if 'update_dir' in user_data:
+        update_dir = user_data['update_dir']
+        remote_version = determine_version(update_dir)
+        if local_version != remote_version:
+            launch_updater()
+    else:
+        update_dir = None
+        launch_updater()
 
     # determine if babelstore is connected and if not
     # launch setup
-    user_data = shelve.open(USER_DATA)
     if 'db_config' in user_data:
         datastore_linked = True
     else:
@@ -51,8 +76,8 @@ if __name__ == '__main__':
         s = Style()
         s.theme_use('xpnative')
         s.configure('.', font=('device', 12))
-        app.iconbitmap('./icons/babel.ico')
-        app.title('Babel v.{}'.format(version))
+        app.iconbitmap('./icons/babel2.ico')
+        app.title('Babel v.{}'.format(local_version))
         app.mainloop()
     else:
         # widget displaying error msg?
