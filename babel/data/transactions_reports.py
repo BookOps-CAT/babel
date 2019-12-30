eaff00aa4a34ed27c44f88724f5d3e11f8f05ad4
klinga <klingaroo@gmail.com>
2019-12-30 09:53:54 -0500

    FY summary logging added

--- babel/data/transactions_reports.py
+++ babel/data/transactions_reports.py
@@ -4,14 +4,13 @@ from datetime import date
 import logging
 
 from pandas import read_sql
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.sql import text
+# from sqlalchemy.exc import IntegrityError
 
 from data.datastore import session_scope
 from data.datastore_worker import construct_fy_summary_stmn
 from reports.reports import generate_fy_summary_for_display
-from logging_settings import format_traceback, LogglyAdapter
-from errors import BabelError
+from logging_settings import LogglyAdapter
+# from errors import BabelError
 
 
 mlogger = LogglyAdapter(logging.getLogger('babel'), None)
@@ -28,9 +27,17 @@ def get_fy_summary(system_id, library_id, user_ids):
         start_date = date.fromisoformat(f'{date_today.year}-07-01')
     end_date = date_today
 
+    mlogger.debug(
+        f'Report criteria: type=FY summary (1), system_id={system_id}, '
+        f'library_id={library_id}, user_ids={user_ids}, '
+        f'start_date={start_date}, end_date={end_date}')
+
     with session_scope() as session:
         stmn = construct_fy_summary_stmn(
             system_id, library_id, user_ids, start_date, end_date)
+
+        mlogger.debug(f'Report query stmn: {stmn}')
+
         df = read_sql(stmn, session.bind)
         data = generate_fy_summary_for_display(df)
 
