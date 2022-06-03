b94479a2098e49437fb580cbe3602eba2a576604
Tomek <klingaroo@gmail.com>
2022-06-03 14:46:41 -0400

    adds mocked vault

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,12 +16,9 @@ def mock_data_dir(tmpdir):
 @pytest.fixture
 def mock_user_data_file(monkeypatch, mock_data_dir):
     u = shelve.open(os.path.join(mock_data_dir, "user_data"))
-    u["db_config"] = {
-        "db_config": "test_app",
-        "host": "test_host",
-        "port": "test_port",
-        "user": "test_user",
-    }
+    u["babelprod"] = b"spam"
+    yield u
+
     u.close()
 
 
@@ -31,9 +28,16 @@ def mock_env_vars(monkeypatch, mock_data_dir):
 
 
 @pytest.fixture
-def fake_vault_creds():
-    keyring.set_keyring(WinVaultKeyring())
-    keyring.set_password("test_app", "test_user", "test_passw")
-    yield
+def mock_vault(monkeypatch):
+    def _patch(*args, **kwargs):
+        return "babel_key"
 
-    keyring.delete_password("test_app", "test_user")
+    monkeypatch.setattr("keyring.get_password", _patch)
+
+
+@pytest.fixture
+def mock_no_vault(monkeypatch):
+    def _patch(*args, **kwargs):
+        return None
+
+    monkeypatch.setattr("keyring.get_password", _patch)
