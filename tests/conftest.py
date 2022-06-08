7c861bd6372c260da73d9e5325bca8993e237139
Tomek <klingaroo@gmail.com>
2022-06-08 14:05:48 -0400

    removes mock_data_dir & mock_user_data_file fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,21 +8,6 @@ from keyring.backends.Windows import WinVaultKeyring
 
 
 @pytest.fixture
-def mock_data_dir(tmpdir):
-    local_data_dir = tmpdir
-    return local_data_dir
-
-
-@pytest.fixture
-def mock_user_data_file(monkeypatch, mock_data_dir):
-    u = shelve.open(os.path.join(mock_data_dir, "user_data"))
-    u["babelprod"] = b"spam"
-    yield u
-
-    u.close()
-
-
-@pytest.fixture
 def mock_env_vars(monkeypatch, mock_data_dir):
     monkeypatch.setenv("LOCALAPPDATA", f"{mock_data_dir}")
 
