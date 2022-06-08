1f7e704bbc3b0865c2319ecb8ff0140cfecfc454
Tomek <klingaroo@gmail.com>
2022-06-08 14:05:29 -0400

    changes installer for middleware creds

--- babel/installer.py
+++ babel/installer.py
@@ -1,5 +1,4 @@
 import ast
-import json
 import logging
 import os
 import time
@@ -15,14 +14,19 @@ from PIL import Image, ImageTk
 try:
     from errors import BabelError
     from gui.utils import BusyManager
-    from credentials import get_from_vault, decrypt_file_data, store_in_vault
+    from credentials import (
+        get_from_vault,
+        decrypt_file_data,
+        store_in_vault,
+        delete_from_vault,
+    )
     from paths import USER_DATA, APP_DATA_DIR, PROD_LOG_PATH
 except ImportError:
     # needed for pytest
-    from .errors import BabelError
-    from .gui.utils import BusyManager
-    from .credentials import get_from_vault
-    from .paths import USER_DATA, APP_DATA_DIR, PROD_LOG_PATH
+    from babel.errors import BabelError
+    from babel.gui.utils import BusyManager
+    from babel.credentials import get_from_vault, delete_from_vault
+    from babel.paths import USER_DATA, APP_DATA_DIR, PROD_LOG_PATH
 
 
 ilogger = logging.getLogger()
@@ -36,7 +40,11 @@ ilogger.addHandler(handler)
 
 
 def has_creds_config_in_user_data(user_data):
-    if "babel" in user_data and user_data["babel"]:
+    if (
+        "db_config" in user_data
+        and isinstance(user_data["db_config"], dict)
+        and user_data["db_config"]
+    ):
         return True
     else:
         return False
@@ -58,7 +66,7 @@ def is_configured():
     Determines if all requried components and credentials
     are present
     """
-    has_data_file = has_data_file()
+    has_data_file = has_user_data_file()
     ilogger.debug(f"Init: user data file located: {has_data_file}")
 
     if has_data_file:
@@ -66,7 +74,9 @@ def is_configured():
         has_db_config = has_creds_config_in_user_data(user_data)
         ilogger.debug(f"Init: found database config data: {has_db_config}")
         try:
-            has_creds = has_creds_in_vault("babelprod", user_data["babelprod"])
+            has_creds = has_creds_in_vault(
+                "babel_db", user_data["db_config"]["DB_USER"]
+            )
             ilogger.debug(f"Init: has credential stored in vault: {has_creds}")
         except KeyError:
             ilogger.error(f"Init: Malformed db_config in user_data.")
@@ -145,6 +155,13 @@ class Installer(Tk):
                 ilogger.debug("Init: creds.bin not found.")
                 self.status.set("credentials file not found.")
 
+            # modify old vault setup
+            delete_from_vault("babelprod", "babelproddbo")
+
+            # clean up existing user_data
+            with shelve.open(USER_DATA) as user_data:
+                user_data.pop("db_config", None)
+
         if creds:
             self.store_creds(creds)
 
@@ -152,12 +169,12 @@ class Installer(Tk):
         try:
             self.status.set("Decrypting...")
             self.statusLbl.update()
-            decrypted = decrypt_file_data(
+            creds = decrypt_file_data(
                 self.key.get().strip().encode("utf8"), "creds.bin"
             )
             ilogger.debug("Init: creds.bin decrypted.")
-            creds = json.loads(decrypted)
             return creds
+
         except BabelError as exc:
             ilogger.error(f"Init: ValueError: {exc}")
             self.status.set(f"Value error. Try again.")
@@ -191,17 +208,30 @@ class Installer(Tk):
         user_data = shelve.open(USER_DATA)
 
         user_data["db_config"] = dict(
-            db_name=creds["db_name"],
-            host=creds["host"],
-            user=creds["user"],
-            port=creds["port"],
+            DB_NAME=creds["DB_NAME"],
+            DB_HOST=creds["DB_HOST"],
+            DB_USER=creds["DB_USER"],
+            DB_PORT=creds["DB_PORT"],
+        )
+        user_data["nyp_platform"] = dict(
+            PLATFORM_OAUTH_SERVER=creds["PLATFORM_OAUTH_SERVER"],
+            PLATFORM_CLIENT_ID=creds["PLATFORM_CLIENT_ID"],
+        )
+        user_data["bpl_solr"] = dict(
+            SOLR_ENDPOINT=creds["SOLR_ENDPOINT"],
         )
         user_data.close()
         ilogger.debug("Init: MySql data stored in the user_data file.")
 
         try:
-            store_in_vault(creds["db_name"], creds["user"], creds["passw"])
-            ilogger.debug("Init: MySql password stored in Windows Credential Manager.")
+            store_in_vault("babel_db", creds["DB_USER"], creds["DB_PASSWORD"])
+            store_in_vault(
+                "babel_platform", creds["PLATFORM_CLIENT_ID"], creds["PLATFORM_SECRET"]
+            )
+            store_in_vault("babel_solr", "babel", creds["SOLR_SECRET"])
+            ilogger.debug(
+                "Init: MySql/Platform/Solr secrets stored in Windows Credential Manager."
+            )
             self.status.set("Success! Closing. Please restart Babel.")
             self.statusLbl.update()
             time.sleep(3)
--- tests/test_installer.py
+++ tests/test_installer.py
@@ -3,31 +3,46 @@ Tests installer application.
 """
 
 import keyring
+import os
 import pytest
+import shelve
 
-from babel.installer import has_creds_config_in_user_data, has_creds_in_vault
+from babel.installer import (
+    has_creds_config_in_user_data,
+    has_creds_in_vault,
+    is_configured,
+)
+from babel.paths import USER_DATA
 
 
-def test_has_creds_config_in_user_data_missing_key(mock_user_data_file):
-    u = mock_user_data_file
-    u.pop("babel", None)
+def test_has_creds_config_in_user_data_missing_key(tmpdir):
+    user_data = shelve.open(os.path.join(tmpdir, "user_data"))
 
-    assert has_creds_config_in_user_data(u) is False
+    assert has_creds_config_in_user_data(user_data) is False
+
+    user_data.close()
 
 
 @pytest.mark.parametrize(
-    "arg,expectation", [("spam", True), (None, False), ("", False)]
+    "arg,expectation", [({"foo": "spam"}, True), (None, False), ({}, False)]
 )
-def test_has_creds_config_in_user_key_validation(mock_user_data_file, arg, expectation):
-    u = mock_user_data_file
-    u["babel"] = arg
-    assert has_creds_config_in_user_data(u) == expectation
+def test_has_creds_config_in_user_key_validation(tmpdir, arg, expectation):
+    user_data = shelve.open(os.path.join(tmpdir, "user_data"))
+    user_data["db_config"] = arg
+
+    assert has_creds_config_in_user_data(user_data) == expectation
+
+    user_data.close()
 
 
 def test_has_creds_in_vault_success(mock_vault):
-    assert has_creds_in_vault("babeldev", "babel") is True
+    assert has_creds_in_vault("babel", "babeldev") is True
 
 
 def test_has_creds_in_vault_fail(mock_no_vault):
     result = keyring.get_password("foo", "bar")
-    assert has_creds_in_vault("babeldev", "babel") is False
+    assert has_creds_in_vault("babeldev", "babeldev") is False
+
+
+def test_is_configured():
+    print(is_configured())
