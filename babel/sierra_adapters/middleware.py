952b0511c27e2bce3c0b66a7b946143e7a7d1dd9
Tomek <klingaroo@gmail.com>
2022-06-14 14:16:46 -0400

    creds location passed to NypPlatform

--- babel/sierra_adapters/middleware.py
+++ babel/sierra_adapters/middleware.py
@@ -3,6 +3,8 @@ This modules handles calls to particular Sierra middleware to discover
 any duplicates in the ILS.
 """
 import os
+import logging
+from requests import Response
 import shelve
 from typing import Optional, Union
 
@@ -12,12 +14,16 @@ from bookops_nypl_platform import PlatformToken, PlatformSession
 
 
 try:
-    from paths import get_user_data_handle
     from credentials import get_from_vault
+    from logging_settings import LogglyAdapter
+    from errors import BabelError
 except ImportError:
     # tests
-    from babel import paths
     from babel.credentials import get_from_vault
+    from babel.errors import BabelError
+    from babel.logging_settings import LogglyAdapter
+
+mlogger = LogglyAdapter(logging.getLogger("babel"), None)
 
 
 def catalog_match(middleware: Union[PlatformSession, SolrSession], keywords: list[str]):
@@ -30,51 +36,173 @@ def catalog_match(middleware: Union[PlatformSession, SolrSession], keywords: lis
 
 
 class NypPlatform(PlatformSession):
-    def __init__(self, library: str) -> None:
+    def __init__(self, library: str, creds_fh: str) -> None:
         """
         Authenticates and opens a session with NYPL Platform.
         Relies on credentials stores in Windows Credential Manager.
 
         Args:
                 library:            'branch' or 'research'
+                creds_fh:           path to user_data `shelve.BsdDbShelf` instance
         """
+        mlogger.info("Initiating session with Platform.")
+
         self.library = library  # branch or research
+        self.creds_fh = creds_fh
+
         if self.library not in ("branch", "research", None):
-            raise ValueError("Invalid library argument passed.")
+            mlogger.error(
+                "Invalid library argument passed to NypPlatform. Unable to open Platfrom session."
+            )
+            raise BabelError("Invalid library argument passed.")
+
+        if not os.path.exists(f"{self.creds_fh}.dat"):
+            mlogger.error("Invalid creds_fh argument passed.")
+            raise BabelError(
+                "Path to user_data not provided. Unable to open Platform session."
+            )
+
         self.agent = "BookOps/Babel"
 
-        client_id, client_secret, oauth_server = self._get_credentials()
-        token = self._get_token(client_id, client_secret, oauth_server)
+        token = self._get_token()
 
         super().__init__(authorization=token, agent=self.agent)
 
-    def _get_credentials(self) -> tuple[str, str, str]:
+    def _get_credentials(
+        self, user_data: shelve.BsdDbShelf
+    ) -> tuple[Optional[str], Optional[str], Optional[str]]:
         """Retrieves NYPL Platform credentials from user_data and vault"""
-        user_data_handle = paths.get_user_data_handle()
-        user_data = shelve.open(user_data_handle)
 
-        config = user_data["nyp_platform"]
-        oauth_server = config["PLATFORM_OAUTH_SERVER"]
-        client_id = config["PLATFORM_CLIENT_ID"]
+        mlogger.debug("Obtaining Platform credentials.")
+        try:
+            config = user_data["nyp_platform"]
+            client_id = config["PLATFORM_CLIENT_ID"]
+            oauth_server = config["PLATFORM_OAUTH_SERVER"]
+            mlogger.debug("Found Platform credentials in user_data.")
+        except KeyError:
+            mlogger.error("Failed to find Platform credentials in user_data.")
+            client_id = None
+            oauth_server = None
+
         client_secret = get_from_vault("babel_platform", client_id)
-
-        user_data.close()
 
         return client_id, client_secret, oauth_server
 
-    def _get_token(
-        self, client_id: str, client_secret: str, oauth_server: str
-    ) -> Optional[PlatformToken]:
+    def _get_token(self) -> Optional[PlatformToken]:
         """Obtains NYPL Platform access token"""
-        token = PlatformToken(client_id, client_secret, oauth_server, self.agent)
+
+        mlogger.debug("Obtaining Platform access token.")
+
+        # check if valid token still available
+        # if not request new one
+        user_data = shelve.open(self.creds_fh)
+
+        try:
+            token = user_data["platform_token"]
+            mlogger.debug("Found Platform access token in user_data.")
+        except KeyError:
+            mlogger.debug("Failed to find access token in user_data.")
+            token = None
+
+        if not token or token.is_expired():
+
+            mlogger.info("Requesting new Platform access token.")
+            client_id, client_secret, oauth_server = self._get_credentials(user_data)
+            token = PlatformToken(client_id, client_secret, oauth_server, self.agent)
+
+        user_data.close()
         return token
 
-    def _parse_response(self, response) -> tuple[Optional[str], Optiona[str]]:
-        pass
+    def _get_bib_nos(self, response: Response) -> list[str]:
+        """
+        Parses Platform response and provides list of Sierra bib numbers of the
+        matches.
 
-    def search(self, keywords: list[str]) -> response[Response]:
-        response = self.get_bib_list(standard_number=keywords)
-        catalog_dup, dup_bibs = self._parse_response(response)
+        Args:
+            response:               `requests.Response` object
+
+        Returns:
+            bib_nos:                list of bib numbers as strings
+        """
+        bib_nos = []
+        data = response.json()["data"]
+        for bib in data:
+            bib_nos.append(bib["id"])
+
+        return bib_nos
+
+    def _find_library_matches(self, bib_nos: list[str]) -> list[str]:
+        """
+        Performs a search to determine if bib on the provided list
+        matches library.
+
+        Args:
+            bib_nos:                list of Sierra bib numbers as strings
+
+        Returns:
+            library_bib_nos
+        """
+        matches = []
+        if self.library is None:
+            return matches
+        else:
+            for bib_no in bib_nos:
+                try:
+                    response = self.check_bib_is_research(id=bib_no)
+                    if response.status_code == 200:
+                        if (
+                            self.library == "research"
+                            and response.json()["is_research"]
+                        ):
+                            matches.append(bib_no)
+                        elif (
+                            self.library == "branch"
+                            and not response.json()["is_research"]
+                        ):
+                            matches.append(bib_no)
+                except BookopsPlatformError as exc:
+                    pass
+        return matches
+
+    def search(self, keywords: list[str]) -> tuple[bool, str]:
+        """
+        Searches NYPL Platform for given ISBNs or UPCs.
+
+        Args:
+            keywords:               list or comma separated string of ISBNs
+                                    or UPCs to search Sierra
+
+        Returns:
+            catalog_dup & dup_bibs
+        """
+        try:
+            response = self.search_standardNos(keywords=keywords, deleted=False)
+            if response.status_code == 200:
+                bib_nos = self._get_bib_nos(response)
+                library_matches = self._find_library_matches(bib_nos)
+                if library_matches:
+                    catalog_dup = True
+                    dup_bibs = ",".join(ibrary_matches)
+            else:
+                catalog_dup = None
+                dup_bibs = None
+        except BookopsPlatformError:
+            catalog_dup = None
+            dup_bibs = None
+
+        return catalog_dup, dup_bibs
+
+    def _store_token(self):
+        if isinstance(self.authorization, PlatformToken):
+            user_data = shelve.open(self.creds_fh)
+            user_data["platform_token"] = self.authorization
+            user_data.close()
+
+    def close(self):
+        # store token for future use before closing the session
+        self._store_token()
+        for v in self.adapters.values():
+            v.close()
 
 
 class BplSolr(SolrSession):
--- tests/unit/test_sierra_adapters.py
+++ tests/unit/test_sierra_adapters.py
@@ -1,36 +1,99 @@
 from contextlib import nullcontext as does_not_raise
+import logging
+import shelve
 
 import pytest
 from bookops_nypl_platform import PlatformToken
 
+
+from babel import paths
+from babel.errors import BabelError
 from babel.sierra_adapters.middleware import NypPlatform
+
+
+from tests.conftest import MockPlatformSessionGetListResponseSuccess
 
 
 @pytest.mark.parametrize("arg", [None, "branch", "research"])
 def test_NypPlatform_valid_library_arg(
-    arg, mock_user_data, mock_vault, mock_platform_post_token_response_200http
+    caplog,
+    arg,
+    dummy_user_data,
+    mock_vault,
+    mock_platform_post_token_response_200http,
 ):
-    with does_not_raise():
-        session = NypPlatform(arg)
+    with caplog.at_level(logging.INFO):
+        with does_not_raise():
+            session = NypPlatform(arg, dummy_user_data)
 
-        assert session.library == arg
-        assert session.agent == "BookOps/Babel"
+            assert session.library == arg
+            assert session.agent == "BookOps/Babel"
+
+    assert "Initiating session with Platform." in caplog.text
 
 
 @pytest.mark.parametrize("arg", ["", "foo", 1])
-def test_NypPlatform_invalid_library_arg(arg, mock_vault):
-    with pytest.raises(ValueError) as exc:
-        NypPlatform(arg)
+def test_NypPlatform_invalid_library_arg(caplog, arg, dummy_user_data, mock_vault):
+    with caplog.at_level(logging.ERROR):
+        with pytest.raises(BabelError) as exc:
+            NypPlatform(arg, dummy_user_data)
 
-    assert "Invalid library argument passed" in str(exc.value)
+        assert "Invalid library argument passed" in str(exc.value)
+
+    assert (
+        f"Invalid library argument passed to NypPlatform. Unable to open Platfrom session."
+        in caplog.text
+    )
 
 
-def test_NypPlatform_get_credentials(mock_platform):
-    result = mock_platform._get_credentials()
-    assert isinstance(result, tuple)
+def test_NypPlatform_get_credentials(caplog, dummy_user_data, mock_platform):
+    user_data = shelve.open(dummy_user_data)
+    with caplog.at_level(logging.DEBUG):
+        result = mock_platform._get_credentials(user_data)
+    assert "Obtaining Platform credentials." in caplog.text
     assert result == ("platform-client", "babel_key", "oauth_server")
 
 
-def test_NypPlatfrom_get_token(mock_platform):
-    token = mock_platform._get_token("platform-client", "babel_key", "oauth_server")
+def test_NypPlatform_get_credentials_user_data_malformed(
+    caplog, dummy_user_data, mock_platform
+):
+    user_data = shelve.open(dummy_user_data)
+    user_data.pop("nyp_platform", None)
+
+    with caplog.at_level(logging.ERROR):
+        result = mock_platform._get_credentials(user_data)
+
+    assert "Failed to find Platform credentials in user_data." in caplog.text
+    assert result == (
+        None,
+        "babel_key",
+        None,
+    )  # because of mocking the vault the second value is not usual None
+    user_data.close()
+
+
+def test_NypPlatform_get_token_in_user_data(caplog, mock_platform):
+    with caplog.at_level(logging.DEBUG):
+        token = mock_platform._get_token()
     assert isinstance(token, PlatformToken)
+    assert "Obtaining Platform access token." in caplog.text
+    assert "Found Platform access token in user_data."
+
+
+def test_NypPlatform_get_token_not_in_user_data(caplog, dummy_user_data, mock_platform):
+    with caplog.at_level(logging.DEBUG):
+        user_data = shelve.open(dummy_user_data)
+        user_data.pop("platform_token", None)
+        user_data.close()
+
+        token = mock_platform._get_token()
+
+    assert "Failed to find access token in user_data." in caplog.text
+    assert "Requesting new Platform access token." in caplog.text
+    assert isinstance(token, PlatformToken)
+
+
+def test_NypPlatform_get_bib_nos(mock_platform):
+    response = MockPlatformSessionGetListResponseSuccess()
+    result = mock_platform._get_bib_nos(response)
+    assert result == ["21776219", "21742979"]
