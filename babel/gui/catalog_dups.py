950a99f6c4ddf379941dab8c80d19c0de927eeaf
Tomek <klingaroo@gmail.com>
2022-06-17 14:54:28 -0400

    init results

--- babel/gui/catalog_dups.py
+++ babel/gui/catalog_dups.py
@@ -5,9 +5,11 @@ from tkinter.ttk import *
 
 from gui.fonts import RFONT, RBFONT, LFONT
 from gui.utils import BusyManager
-from logging_settings improt LogglyAdapter
-from reports.cart import format_nypl_sierra_data_for_display, format_bpl_sierra_data_for_display
-from sierra_adapters.middleware import catalog_lookup
+from logging_settings import LogglyAdapter
+from reports.cart import (
+    format_nypl_sierra_data_for_display,
+    format_bpl_sierra_data_for_display,
+)
 
 mlogger = LogglyAdapter(logging.getLogger("babel"), None)
 
@@ -15,9 +17,9 @@ mlogger = LogglyAdapter(logging.getLogger("babel"), None)
 class CatalogDupWidget:
     """Displays Sierra dup information"""
 
-    def __init__(self, parent, **app_data):
+    def __init__(self, parent, dups_data):
         self.parent = parent
-        self.app_data = app_data
+        self.dups_data = dups_data
 
         mlogger.debug("CartDupWidet activated.")
 
@@ -31,16 +33,31 @@ class CatalogDupWidget:
         frm = Frame(self.top)
         frm.grid(row=0, column=0, sticky="snw")
 
+        self.resultsTxt = Text(
+            frm,
+            width=65,
+            state=("disabled"),
+            wrap=WORD,
+            background="SystemButtonFace",
+            borderwidth=0,
+        )
+        self.resultsTxt.grid(row=0, column=0, sticky="nsw")
+
         scrollbar = Scrollbar(frm, orient=VERTICAL)
-        scrollbar.grid(row=0, column=0, sticky="snw")
-        # scrollbar.["command"] = self.frm.y
+        scrollbar.grid(row=0, column=1, sticky="snw")
 
-        # self.resultsTxt = Text()
+        self.resultsTxt.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.resultsTxt.yview)
 
-        # display nice msg if no Sierra/middleware data availalbe
+        self.display_data()
 
-    def query_middleware(self):
-        # retrieve resource data from db based on resource.did
-        # perform middleware query based on dup_bibs data
-        # format that data for display
-        # display
+    def display_data(self):
+        self.resultsTxt["state"] = "normal"
+        for bib_data, items_data in self.dups_data:
+            self.resultsTxt.insert(END, f"{bib_data['bibNo']}\n")
+            self.resultsTxt.insert(END, f"{bib_data['title']} / {bib_data['author']}\n")
+            self.resultsTxt.insert(
+                END, f"{bib_data['pubPlace']} - {bib_data['pubDate']}"
+            )
+
+        self.resultsTxt["state"] = "disable"
