a69b98b435a6bac38b910855ad938efdb776ad43
Tomek <klingaroo@gmail.com>
2022-06-28 13:52:28 -0400

    adds solr search

--- babel/sierra_adapters/solr.py
+++ babel/sierra_adapters/solr.py
@@ -8,6 +8,7 @@ from typing import Optional
 
 from bookops_bpl_solr import SolrSession
 from bookops_bpl_solr.session import BookopsSolrError
+from requests import Response
 
 try:
     from credentials import get_from_vault
@@ -75,3 +76,63 @@ class BplSolr(SolrSession):
             raise BabelError("Unable to obtain BPL Solr credentials.")
         else:
             return endpoint, client_key
+
+    def _get_bib_nos(self, response: Response) -> list[str]:
+        """
+        Parses Solr response and returns matching Sierra bib #s.
+
+        Args:
+            response:               `requests.Response` instance
+
+        Returns:
+            bib_nos
+        """
+        return [bib["id"] for bib in response.json()["response"]["docs"]]
+
+    def search(self, keywords: list[str], keyword_type: Optional[str] = "isbn"):
+        """
+        Searches BPL Solr for given ISBN or UPC.
+
+        Args:
+            keywords:               list of ISBNs or UPCs
+            keyword_type:           'isbn' or "upc"
+
+        Returns:
+            catalog_dup & dup_bibs
+        """
+        try:
+            if keyword_type == "isbn":
+                response = self.search_isbns(
+                    keywords,
+                    default_response_fields=False,
+                    response_fields="id",
+                )
+            elif keyword_type == "upc":
+                response = self.search_upc(
+                    keywords, default_response_fields=False, response_fields="id"
+                )
+            else:
+                mlogger.warning(
+                    "Attempting unsupported search in BPL Solr. Only ISBN and UPC searches are permitted."
+                )
+                response = None
+                catalog_dup = False
+                dup_bibs = None
+
+            if response and response.status_code == 200:
+                bib_nos = self._get_bib_nos(response)
+                if bib_nos:
+                    catalog_dup = True
+                    dup_bibs = ",".join(bib_nos)
+                else:
+                    catalog_dup = False
+                    dup_bibs = None
+            else:
+                catalog_dup = False
+                dup_bibs = None
+        except BookopsSolrError:
+            mlogger.warning("Encountered problem with Solr request. Skipping.")
+            catalog_dup = False
+            dup_bibs = None
+
+        return catalog_dup, dup_bibs
--- tests/unit/test_sierra_adapters_solr.py
+++ tests/unit/test_sierra_adapters_solr.py
@@ -8,6 +8,8 @@ import pytest
 from babel.sierra_adapters.solr import BplSolr
 from babel.errors import BabelError
 
+from tests.conftest import MockSolrSessionResponseSuccess
+
 
 def test_invalid_creds_fh(caplog):
     with caplog.at_level(logging.ERROR):
@@ -65,3 +67,39 @@ def test_get_creds_missing_client_key_in_vault(caplog, dummy_user_data, mock_no_
 
         assert "Unable to obtain BPL Solr client-key from vault." in caplog.text
         assert "Unable to obtain BPL Solr credentials." in str(exc.value)
+
+
+def test_get_bib_nos_success(dummy_user_data):
+    response = MockSolrSessionResponseSuccess()
+    solr = BplSolr(dummy_user_data)
+
+    assert solr._get_bib_nos(response) == ["11499389", "11499399"]
+
+    solr.close()
+
+
+@pytest.mark.parametrize("arg", ["isbn"])
+def test_search_success(dummy_user_data, mock_solr_search_success, mock_vault, arg):
+    with BplSolr(dummy_user_data) as solr:
+        result = solr.search(["1419864173"], arg)
+
+    assert result == (True, "11499389,11499399")
+
+
+def test_search_not_found(dummy_user_data, mock_vault, mock_solr_search_not_found):
+    with BplSolr(dummy_user_data) as solr:
+        result = solr.search(["1419864173"], "isbn")
+
+    assert result == (False, None)
+
+
+def test_search_unsupported(caplog, dummy_user_data, mock_vault):
+    with caplog.at_level(logging.WARNING):
+        with BplSolr(dummy_user_data) as solr:
+            result = solr.search(["1419864173"], None)
+
+    assert (
+        "Attempting unsupported search in BPL Solr. Only ISBN and UPC searches are permitted."
+        in caplog.text
+    )
+    assert result == (False, None)
