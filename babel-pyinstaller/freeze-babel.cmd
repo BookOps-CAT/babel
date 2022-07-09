b2a73fe6300bd7703e45db90af7fdf67e334ce2f
klinga <klingaroo@gmail.com>
2022-07-09 12:13:06 -0400

    remomves version.txt file and bumps version to 4.0.0

--- babel-pyinstaller/freeze-babel.cmd
+++ babel-pyinstaller/freeze-babel.cmd
@@ -6,7 +6,6 @@ pyinstaller --clean --windowed ^
     --hidden-import="sqlalchemy.ext.baked" ^
     --icon=".\icons\babel2.ico" ^
     --add-data="./icons;icons" ^
-    --add-data="version.txt;." ^
     --add-data="creds.bin;." ^
     --version-file=win_info.txt ^
     babel.py
