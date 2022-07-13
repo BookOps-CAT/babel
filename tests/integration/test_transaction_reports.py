5aea34229536894f44f543241c4e310cc3c48441
Tomek <klingaroo@gmail.com>
2022-07-13 08:01:11 -0400

    adds basic test to asserrt query2dataframe returns dataframe instance

--- tests/integration/test_transaction_reports.py
+++ tests/integration/test_transaction_reports.py
@@ -1,6 +1,8 @@
+from pandas import DataFrame
+
 from babel.data.transactions_reports import query2dataframe
 
 
 def test_query2dataframe(dev_user_data, mock_db_creds_from_vault):
-    df = query2dataframe(None, None, [], "2022-04-01", "2022-06-30")
-    print(df.head())
+    result = query2dataframe(None, None, [], "2022-04-01", "2022-06-30")
+    assert isinstance(result, DataFrame)
