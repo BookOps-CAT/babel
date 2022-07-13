69b1d29f794352f96a531be2d1290cb4485c71db
Tomek <klingaroo@gmail.com>
2022-07-13 11:32:35 -0400

    fixes BPL Sierra ID export header & blackened (#170)

--- babel/ingest/sierra_exports.py
+++ babel/ingest/sierra_exports.py
@@ -4,8 +4,8 @@ import csv
 from errors import BabelError
 
 
-BPL_IDS_HEADER = ['VEN TITL #', 'RECORD #', 'RECORD #']
-NYPL_IDS_HEADER = ['VEN TITL #', 'RECORD #(ORDER)', 'RECORD #(BIBLIO)']
+BPL_IDS_HEADER = ["VEN TITL #", "RECORD #(Order)", "RECORD #(Bibliographic)"]
+NYPL_IDS_HEADER = ["VEN TITL #", "RECORD #(ORDER)", "RECORD #(BIBLIO)"]
 
 
 def get_sierra_ids(source_fh, system_id):
@@ -19,23 +19,25 @@ def get_sierra_ids(source_fh, system_id):
     """
 
     ids = []
-    with open(source_fh, 'r') as file:
-        reader = csv.reader(file, delimiter=',', quotechar='"')
+    with open(source_fh, "r") as file:
+        reader = csv.reader(file, delimiter=",", quotechar='"')
         header = reader.__next__()
 
         # verify Sierra export has a correct format
         if system_id == 1:
             if not header == BPL_IDS_HEADER:
                 raise BabelError(
-                    'Exported from Sierra IDs should be in\n'
-                    'the following order:\n\t'
-                    '{}'.format('\n\t'.join(BPL_IDS_HEADER)))
+                    "Exported from Sierra IDs should be in\n"
+                    "the following order:\n\t"
+                    "{}".format("\n\t".join(BPL_IDS_HEADER))
+                )
         elif system_id == 2:
             if not header == NYPL_IDS_HEADER:
                 raise BabelError(
-                    'Exported from Sierra IDs should be in\n'
-                    'the following order:\n\t'
-                    '{}'.format('\n\t'.join(NYPL_IDS_HEADER)))
+                    "Exported from Sierra IDs should be in\n"
+                    "the following order:\n\t"
+                    "{}".format("\n\t".join(NYPL_IDS_HEADER))
+                )
 
         for row in reader:
             if len(row) == 3:
